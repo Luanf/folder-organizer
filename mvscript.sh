87f0ffa3b0eefbbb15b03204033f99c5766c788c
Luan <luanegidioferreira@gmail.com>
2017-09-23 16:25:05 -0300

    added code for more types

--- mvscript.sh
+++ mvscript.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 #
 
-SIMPLE_TEXT="txt|txz"
-for file in ~/Desktop/tmp/*
+SIMPLE_TEXT="txt|md"
+DOCS="pdf|ppt|doc|"
+ARCHIVE="zip|tgz"
+CODE="c|py|java"
+IMAGE="png|gif"
+MULTIMEDIA="mp3|mp4"
+INSTALLER="dmg"
+WEB="html"
+
+for file in ~/Downloads/*
 do
-	if [[ $file =~ .*\.($SIMPLE_TEXT) ]]
-		then
-		echo $file
+	if [[ $file =~ .*\.($SIMPLE_TEXT) ]]; then
+		
+	elif [[ $file =~ .*\.($DOCS) ]]; then
+
+	elif [[ $file =~ .*\.($ARCHIVE) ]]; then
+
+	elif [[ $file =~ .*\.($CODE) ]]; then
+
+	elif [[ $file =~ .*\.($IMAGE) ]]; then
+
+	elif [[ $file =~ .*\.($MULTIMEDIA) ]]; then
+
+	elif [[ $file =~ .*\.($INSTALLER) ]]; then
+
+	else [[ $file =~ .*\.($WEB) ]]; then
+
 	fi
 done
